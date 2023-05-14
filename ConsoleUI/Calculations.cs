6abef25e225ad2a86fa8b9484f024b285f305c39
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-13 23:20:23 -0600

    code complete.

---
+++ ConsoleUI/Calculations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferentUI;
+
+public static class Calculations
+{
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using ConsoleUI.Models;
+using DifferentUI;
+
+PersonModel person = new PersonModel();
+
+Calculations.Add(4, 3);
